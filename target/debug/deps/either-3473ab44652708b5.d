b4af635e2012cbf5c6ccee6bff5d122015de1d52
AllanJRY <jarry.allan@gmail.com>
2023-12-01 09:46:49 +0100

    fix(day-01): add .gitignore

--- target/debug/deps/either-3473ab44652708b5.d
+++
@@ -1,5 +0,0 @@
-/home/allan/dev/advent_of_code/2023/target/debug/deps/either-3473ab44652708b5.rmeta: /home/allan/.cargo/registry/src/index.crates.io-6f17d22bba15001f/either-1.9.0/src/lib.rs
-
-/home/allan/dev/advent_of_code/2023/target/debug/deps/either-3473ab44652708b5.d: /home/allan/.cargo/registry/src/index.crates.io-6f17d22bba15001f/either-1.9.0/src/lib.rs
-
-/home/allan/.cargo/registry/src/index.crates.io-6f17d22bba15001f/either-1.9.0/src/lib.rs:
